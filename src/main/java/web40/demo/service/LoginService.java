9e63b72bb162bf1ffd7088930061a27ad0192882
BuildTools <unconfigured@null.spigotmc.org>
2023-04-23 02:20:44 -0300

    main

---
+++ src/main/java/web40/demo/service/LoginService.java
@@ -0,0 +1,25 @@
+/* package web40.demo.service;
+
+import com.example.pruebita.model.User;
+import com.example.pruebita.repository.LoginRepository;
+import com.example.pruebita.repository.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+    @Autowired
+    private LoginRepository loginRepository;
+
+    public void addUser(User user){
+        loginRepository.save(user);
+    }
+
+    public boolean checkLogin(User user){
+        User currentUser = loginRepository.findByMailUser(user.getMailUser());
+        if ( currentUser != null){
+            return user.getPasswordUser().equals(currentUser.getPasswordUser()) ;
+        }
+        else  return false;
+    }
+} */
