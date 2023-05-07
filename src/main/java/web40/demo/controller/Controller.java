6741134d4c1bfd349f312f8083bdd730cfb5f19b
BuildTools <unconfigured@null.spigotmc.org>
2023-05-06 22:32:12 -0300

    cors6

--- src/main/java/web40/demo/controller/Controller.java
+++ src/main/java/web40/demo/controller/Controller.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins ={"https://jay-portfolio-316f2.web.app"})
+@CrossOrigin(origins ="https://jay-portfolio-316f2.web.app")
 public class Controller { 
 
 @Autowired
