d8ff0602f73c8e4bd5c21bd6625066c82be8eb8e
BuildTools <unconfigured@null.spigotmc.org>
2023-05-07 12:56:13 -0300

    login con jpaRepository en vez de crudRepository

--- src/main/java/web40/demo/repository/LoginRepository.java
+++ src/main/java/web40/demo/repository/LoginRepository.java
@@ -1,10 +1,10 @@
 package web40.demo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import web40.demo.model.User;
 
 @Repository
-public interface LoginRepository extends CrudRepository<User, Integer> {
+public interface LoginRepository extends JpaRepository<User, Integer> {
      User findByMailUser(String mailUser);
 }
